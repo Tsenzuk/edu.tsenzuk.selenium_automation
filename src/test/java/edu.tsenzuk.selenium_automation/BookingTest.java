8a6491f671f66745fc3dda554bd5ef93559ae791
Sergey <tsenzuk@gmail.com>
2016-12-05 19:10:21 +0200

    init commit

---
+++ src/test/java/edu.tsenzuk.selenium_automation/BookingTest.java
@@ -0,0 +1,15 @@
+package edu.tsenzuk.selenium_automation;
+
+import org.junit.Test;
+
+public class BookingTest {
+    @Test
+    public void testDownloadLinkExists() {
+
+    }
+
+    @Test
+    public void testTabsExist() {
+
+    }
+}
